602c5424a2c876d994bbb37300b120c44d47cd8d
Vinicius Liberatti Bovo <viniciusliberattibovo@gmail.com>
2026-09-09 10:50:25 -0300

    Quarto Commit - Portal DSM

--- src/pages/Cursos.jsx
+++ src/pages/Cursos.jsx
@@ -16,6 +16,18 @@ function Cursos() {
  titulo="Programação"
  descricao="Lógica e desenvolvimento."
  />
+ <Card
+ titulo="Engenharia de Software"
+ descricao="Processos, requisitos e boas práticas."
+ />
+ <Card
+ titulo="Estrutura de Dados"
+ descricao="Listas, pilhas, filas e árvores."
+ />
+ <Card
+ titulo="Desenvolvimento Mobile"
+ descricao="Aplicações para Android e iOS."
+ />
  </section>
  </main>
  );
